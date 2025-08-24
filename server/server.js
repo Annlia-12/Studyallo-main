317e0d186ea117161289143cb89ff619c79982f7
Annlia Jose <annliajose29@gmail.com>
2025-08-24 14:20:45 +0530

    update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const pdfParse = require("pdf-parse");
 const mammoth = require("mammoth");
 require("dotenv").config();
 
-const OpenAI = require("openai");
+const OpenAI = require("openai/index.js");
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
